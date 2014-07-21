f2ddc80442b6c3716dbaf54c8625b9c17a910c96
emanuele <emanuele45@gmail.com>
2014-07-22 00:07:45 +0200

    Rewrite of the addon in form of SCEditor plugin

--- README.md
+++ README.md
@@ -5,30 +5,9 @@ With this mod it is possible to use all the available smileys without leave the 
 
 If you want to help, feel free!<br />
 The mod is now available at github:<br />
-https://github.com/emanuele45/Tabbed-Smileys<br />
-with a brand now BSD license attached to it! :P
+https://github.com/emanuele45/Tabbed-Smileys
 
 ---------------------------------------
-
-### **NOTE:** version 0.3.99b is a BETA version!
-
-In this version I added the possibility to move (almost) all the elements of the posting interface into "tabs". It means that the formatting buttons, the attachments, the "other settings" are all moved on top of the post area each in a separated tab.<br />
-Once installed you will find a new drop down box in *admin > Forum > Posts and Topics > Post Settings* with 3 options:
-
-* Completely off (i.e. the classic SMF interface)
-* Only smileys in tabs (i.e. the usual behaviour of the mod, only the smiley are put in a tab-like interface)
-*All posting elements in tabs (i.e. the behaviour described just above)
-
-Once selected the last option, a new checkbox will appear giving the possibility to chose to show the smileys' tabs in a separated row.<br />
-**It _should_ work, but I didn't test it that much, so feel free to use it, but be ready to face problems. ;)**<br />
-That said: I wait your feedback: is it useful? Is it useless? Feel free to test it and report problems or suggestions to improve it!<br />
-_The changes affect only SMF 2.0_, I won't bring them to SMF 1.1.x.
-
----------------------------------------
-
-### Important note for 1.1.x users
-
-To install the mod on SMF 1.1.15 and newer versions, please use the [Version Emulate](http://custom.simplemachines.org/mods/index.php?mod=2113) mod.
 
 ## Configuration
 
@@ -42,14 +21,4 @@ It's also possible to define a custom name for each tab and change the css style
 
 ## Change log
 
-* _0.3.7_: fixed utf8, update to SMF versions 1.1.14 and 2.0
-* _0.3.6_:
-  * fixed a bug for SMF 2.0 stopping js execution in some situations and bad behaviour with WYSIWYG editor (i.e. smiley not working...). Many thanks to [abraamz](http://www.simplemachines.org/community/index.php?topic=417533.msg2978622#msg2978622) for reporting and support in beta testing!
-  * added polish UTF-8 translation (thanks to [becometa(http://www.simplemachines.org/community/index.php?topic=417533.msg2969650#msg2969650)) and english-british ISO/UTF-8 and english and italian UTF-8. **Note:** since I have always problems with characters encoding there I expect problems here too... :P). Thanks [Arantor](http://www.simplemachines.org/community/index.php?topic=417533.msg2969652#msg2969652) for the hint.
-* _0.3.5_: fixed the problem of loading tabs only when used for SMF 2.0 (I hope)
-* _0.3.4_: removed "on demand" loading of tabs content for SMF 2.0 due to a bug reported by [RAULVK](http://www.simplemachines.org/community/index.php?topic=417533.msg2968622#msg2968622)
-* _0.3.3_: fixed a bug with SMF 2.0 and IE (double "more" button) [reported by Rifugio](http://www.simplemachines.org/community/index.php?topic=417533.msg2965693#msg2965693) (thanks!)
-* _0.3.2_: now the tabs' content is loaded only "on demand"; added an option to hide the "more" button/link
-* _0.3.1_: updated for SMF 1.1.13 and SMF 2.0 RC5
-* _0.3.0_: improved flexibility with the possibility to decide which smiley should go in which tab, assign names to the tabs and change the css style
-* _0.2.3_: initial release particular behaviour: by default the number of tabs is determined by the number of smileys in the post form but it can also be defined in the Smiley and Message Icons settings page of the admin panel.
+* 0.1.0 - Complete rewrite for ElkArte and SCEditor - added only basic features
---
+++ tabbedSmiley.js
@@ -0,0 +1,150 @@
+/**
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0 Release Candidate 2
+ * Extension functions to provide ElkArte compatibility with sceditor
+ */
+
+(function($, window, document) {
+	'use strict';
+	var tabs = {}, tab_btns = {};
+
+		/**
+		 * Create the button of a tab
+		 */
+		function createBtn(idx, target, text, cssclass, callback)
+		{
+			tab_btns[idx] = $('<li id="' + idx + '" ' + (cssclass ? 'class="sceditor-tabs-' + cssclass + '"' : '') + ' />')
+				.data('target', target);
+			if (callback)
+				tab_btns[idx].click(function (e) {
+					e.preventDefault();
+					callback(e);
+				});
+			else
+				tab_btns[idx].click(function (e) {
+					var cur_id = $(this).attr('id');
+					e.preventDefault();
+
+					$.each(tabs, function(idx, val) {
+						$(tab_btns[idx]).removeClass('sceditor-tabs-active');
+						$(this).removeClass('visible').addClass('hidden');
+
+						if ($(this).attr('id') == cur_id + '-tab' && !tab_btns[idx].added)
+							$('#sceditor-smileycontainer').after(tabs[idx]);
+					});
+
+					$(this).addClass('sceditor-tabs-active');
+					$('#' + $(this).data('target')).removeClass('hidden').addClass('visible').find('img').each(function () {
+						$(this).attr('src', $(this).attr('src-url'));
+					});
+				});
+
+				tab_btns[idx].text(base._(text));
+
+				return tab_btns[idx];
+		}
+
+		/**
+		 * Create the image of an emoticon (with events)
+		 */
+		function buildEmoticon(code, emoticon) {
+			if (emoticon === '')
+				return false;
+			else
+				return $('<img />')
+					.attr({
+						'src-url': emoticon.url || emoticon,
+						alt: code,
+						title: emoticon.tooltip || emoticon
+					})
+					.click(function (e) {
+						var start = '',
+							end = '';
+
+						if (base.opts.emoticonsCompat)
+						{
+							start = '<span> ';
+							end   = ' </span>';
+						}
+
+						if (base.inSourceMode())
+							base.sourceEditorInsertText(' ' + $(this).attr('alt') + ' ');
+						else
+							base.wysiwygEditorInsertHtml(start + '<img src="' + $(this).attr("src") + '" data-sceditor-emoticon="' + $(this).attr('alt') + '" />' + end);
+
+						e.preventDefault();
+					});
+		}
+
+		/**
+		 * Attach the tabbed interface
+		 */
+		function addTabbed() {
+			var $btn_tabs = null,
+				$tabs = null;
+
+			var emoticons = $.extend({}, base.opts.emoticons.dropdown),
+				popup_exists = false,
+				smiley_popup = '',
+				$editor = $('#editor_toolbar_container'),
+				content = $('<div class="sceditor-insertemoticon button" />');
+
+			for (smiley_popup in base.opts.emoticons.popup)
+			{
+				popup_exists = true;
+				break;
+			}
+
+			// For any smileys that go in the more popup
+			if (popup_exists)
+			{
+				var emots = $.extend({}, base.opts.emoticons.popup);
+
+				if ($btn_tabs === null)
+				{
+					var count = 0, ccount = 0;
+
+					// On these two we build our system
+					$btn_tabs = $('<ul class="sceditor-emot-head-tabs" />');
+					$tabs = $('<ul class="sceditor-emot-tabs" />');
+
+					$btn_tabs.append(createBtn('sceditor-smileycontainer-top', 'sceditor-smileycontainer', 'Base', 'active'));
+					tabs['sceditor-smileycontainer'] = $('#sceditor-smileycontainer');
+
+					$.each(emots, function (idx, val) {
+						if (count == 0 || idx == '-0')
+						{
+							// Prepare a new panel
+							tabs['sceditor-tabs-' + count] = $('<div id="sceditor-tabs-' + count + '-tab" class="hidden" />');
+							ccount = count;
+							$btn_tabs.append(createBtn('sceditor-tabs-' + count++, 'sceditor-tabs-' + ccount + '-tab', 'Tab ' + count));
+						}
+
+						tabs['sceditor-tabs-' + ccount].append(buildEmoticon(idx, val));
+					});
+					content.before($btn_tabs);
+
+				$editor.find('.sceditor-insertemoticon').before($btn_tabs);
+				}
+			}
+		}
+
+	/**
+	 * Mentioning plugin interface to SCEditor
+	 *  - Called from the editor as a plugin
+	 *  - Monitors events so we control the elk_mention
+	 */
+	$.sceditor.plugins.tabbedemotes = function() {
+		var base = this;
+
+		/**
+		 * Initialize, called when sceditor starts and initializes plugins
+		 */
+		base.signalReady = function() {
+			addTabbed();
+		};
+	};
+})(jQuery, window, document);
